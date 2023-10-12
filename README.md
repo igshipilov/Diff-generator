ec1c29a802d1be8d449474e33910f26398dd1005
Igor <igor_shipilov@inbox.ru>
2023-10-12 13:02:23 +0700

    first version working in CLI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/igshipilov/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/igshipilov/frontend-project-46/actions)
+
+### How it works
+<a href="https://asciinema.org/a/613520?autoplay=1"><img src="https://asciinema.org/a/613520.png" width="836"/></a>
--- gendiff.js
+++ gendiff.js
@@ -4,6 +4,8 @@ import { program } from "commander";
 import path from 'path';
 // import { cwd } from 'node:process'; // почему-то `process.cwd` работает без импорта `cwd`
 import fs from 'fs';
+import { getParsedFile } from "./parse.js";
+import _ from 'lodash';
 
 
 
@@ -38,22 +40,71 @@ const getFilePath = (fileName) => {
   return filePath;
 };
 
+console.log(getFilePath('file1.json'))
+
 const getFileContent = (file) => fs.readFileSync(getFilePath(file), 'utf-8');
 
 
-const genDiff = () => {
-  program
+const genDiff = (file1, file2) => {
+  const obj1 = _.cloneDeep(getParsedFile(file1));
+  const obj2 = _.cloneDeep(getParsedFile(file2));
+
+  const comparison = {};
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  // const values1 = Object.values(obj1);
+  // const values2 = Object.values(obj2);
+
+  // const entries1 = Object.entries(obj1);
+  // const entries2 = Object.entries(obj2);
+
+  const keys = _.union(keys1, keys2).sort(); // Массив со списком всех ключей из обоих объектов
+
+  const indent = ' ';
+  const indentCount = 2;
+  const currentIndent = indent.repeat(indentCount);
+
+  // Итерируем массив ключей, проверяя наличие каждого ключа в переданных объектах и формируя diff строку
+  const lines = keys
+    .map((key) => {
+        if (!Object.hasOwn(obj2, key)) {
+          return `${currentIndent}- ${key}: ${obj1[key]}`;
+
+        } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+          return `${currentIndent}${genDiff(obj1[key], obj2[key])}`;
+
+        } else if (!Object.hasOwn(obj1, key)) {
+          return `${currentIndent}+ ${key}: ${obj2[key]}`;
+
+        } else if (obj1[key] !== obj2[key]) {
+          return `${currentIndent}- ${key}: ${obj1[key]}\n${currentIndent}+ ${key}: ${obj2[key]}`;
+
+        } else if (obj1[key] === obj2[key]) {
+          return `${currentIndent}${currentIndent}${key}: ${obj1[key]}`;
+
+        }
+    });
+
+  console.log(['{',
+    ...lines,
+    '}'
+  ].join('\n'));
+};
+
+// console.log(genDiff('file1.json', 'file2.json'))
+
+program
   .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action(getFilePath)
+  .action(genDiff)
 
 program.parse();
-}
 
-genDiff();
 
 // program
 //   .name('gendiff')
@@ -65,4 +116,4 @@ genDiff();
 
 // program.parse();
 
-export default genDiff;
+export { genDiff, getFileContent };
